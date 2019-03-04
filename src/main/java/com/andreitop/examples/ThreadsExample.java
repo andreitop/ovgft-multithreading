06622dd665afd5a629e27cb48585f121f4535339
Andrei Dremov <andrei_dremov@epam.com>
2019-03-04 03:00:48 +0300

    Add thread creation example

--- src/main/java/com/andreitop/examples/ThreadsExample.java
+++ src/main/java/com/andreitop/examples/ThreadsExample.java
@@ -1,8 +1,41 @@
 package com.andreitop.examples;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadsExample {
 
-	public static void main(String[] args) {
-		System.out.println(Thread.currentThread());
-	}
+  public static void main(String[] args) {
+    System.out.println(Thread.currentThread());
+
+    Runnable runnable = () -> System.out.println("Runnable: " + Thread.currentThread() + " " + Thread.currentThread().getId());
+    Thread thread1 = new Thread(runnable);
+    thread1.start();
+
+    TestThread testThread = new TestThread();
+    testThread.setName("main");
+    testThread.start();
+  }
+
+
+  private static void showDump() throws InterruptedException {
+    System.out.println(Thread.currentThread());
+    int[][] array = new int[10000][];
+
+    for (int i = 0; i < array.length; i++) {
+      array[i] = new int[100000];
+      array[i][0]++;
+      TimeUnit.MILLISECONDS.sleep(30);
+    }
+    System.out.println(Arrays.deepToString(array));
+  }
+
+}
+
+class TestThread extends Thread {
+
+  @Override
+  public void run() {
+    System.out.println("Extends thread: " + Thread.currentThread());
+  }
 }
